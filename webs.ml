645bd0a35e32a54b51e4a0eefafec4552af92519
karitra <karev.alexandre@gmail.com>
2016-05-26 15:38:04 +0300

    github release

--- webs.ml
+++ webs.ml
@@ -1,7 +1,7 @@
 
 open Core
 open Core.Std
-(* open Re2 *)
+open Re2
 
 module Regex = Re2.Regex
 
@@ -20,13 +20,20 @@ let bf_size = 16 * 1024
 let els_per_page = 25
 
 let gen_thumb_page rpath off =
+	Printf.printf "GET %s\n" rpath;
 	let path = Sys.getcwd () ^ "/" ^ rpath in
 	let bf = Buffer.create bf_size in
 	let bf_app = Buffer.add_string bf in
 	let fls =
 		let rg = Regex.create_exn "^(\\d+)_(\\d+)\\.tiff$" in
-		let matcher = Regex.matches rg in
-			path |> Sys.ls_dir |> List.filter ~f:matcher
+		let matcher = Regex.matches rg in match Sys.is_directory path with
+			| `No | `Unknown  -> print_endline "path not a dir"; Array.create 0 ""
+			| `Yes -> let filtered_tiff_array =
+						path
+						|> Sys.ls_dir 
+						|> List.filter ~f:matcher 
+						|> List.to_array in 
+						Array.slice filtered_tiff_array off (off + els_per_page)
 	in
 	let gen_html_in_bf fl =
 		begin
@@ -38,8 +45,7 @@ let gen_thumb_page rpath off =
 				bf_app "<html><head><title>Images thumbnails</title></head><body>";
 				bf_app (Printf.sprintf "<h3>Listing for path %s</h3>" rpath);
 				bf_app "<table>";
-				(* List.iter ~f:(fun x -> bf_app x) fls; *)
-				List.iter ~f:gen_html_in_bf fls;
+				Array.iter ~f:gen_html_in_bf fls;
 				bf_app "</table>";
 				bf_app "</table></body></html>";
 				Buffer.contents bf
@@ -48,11 +54,8 @@ let gen_thumb_page rpath off =
 let srv =
 	let callback conn req body = 
 		let url  = req |> Request.uri in
+		print_endline ("path: " ^ (url |> Uri.path));
 		let path = url |> Uri.path in
-		(* let uri  = url |> Uri.to_string in
-    	let meth    = req |> Request.meth    |> Code.string_of_method in
-    	let headers = req |> Request.headers |> Header.to_string      in
-    	let files = Sys.readdir path in *)
     		body |> Cohttp_lwt_body.to_string 
     			>|= (fun body -> gen_thumb_page path 0)
     			>>= (fun body -> Server.respond_string ~status:`OK ~body ()) in 
