cd6ec667782b2caa276654aca9f21800aa7f07f6
karitra <karev.alexandre@gmail.com>
2016-07-31 22:52:39 +0300

    script to run solver over folder

--- run.solver.sh
+++ run.solver.sh
@@ -3,5 +3,5 @@
 CPUs=`nproc --all`
 ROLLS="1_200_000"
 
-find $1 -name '*.vecs' | xargs -P "$CPUs" -I '{SUBJ}' sh -c "./optimal_seq.native -file {SUBJ} -rolls $ROLLS > {SUBJ}.path"
+find $1 -name '*.vecs' | xargs -P "$CPUs" -I '{SUBJ}' sh -c "./optimal_seq.native -file {SUBJ} -rolls $ROLLS > $1/{SUBJ}.path"
 
