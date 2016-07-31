f7aeb58639e182617e3b56bf9d56a92d595770e6
karitra <karev.alexandre@gmail.com>
2016-07-31 16:25:51 +0300

    unsafe flag

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ corebuild -j 4 -use-ocamlfind -tag thread -pkgs cohttp.lwt,base64,re2,lablgtk2,c
 # TODO: remove camlimages from packages
 #
 echo Building vectors generator...
-corebuild -j 4 -use-ocamlfind -tag thread -pkgs ctypes.foreign,camlimages.tiff,re2 gen_vec.native
+corebuild -j 4 -cflags -unsafe -use-ocamlfind -tag thread -pkgs ctypes.foreign,camlimages.tiff,re2 gen_vec.native
 # corebuild -j 4 -use-ocamlfind -tag thread -pkgs camlimages.tiff,re2 gen_vec.byte
 
 echo Building MC solver...
-corebuild -j 4 -use-ocamlfind -tag thread -pkgs lacaml optimal_seq.native
+corebuild -j 4 -cflags -unsafe -use-ocamlfind -tag thread -pkgs lacaml optimal_seq.native
 
 # echo Building image loader tests...
 # corebuild -j 3 -use-ocamlfind -tag thread -pkgs ctypes.foreign -lflags -cclib,-ltiff  vec_of_tiff.native
