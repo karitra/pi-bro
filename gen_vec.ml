62116fb3278023250d78a57852fc98d7238e71dc
karitra <karev.alexandre@gmail.com>
2016-07-31 15:22:47 +0300

    added frame id to vector file

--- gen_vec.ml
+++ gen_vec.ml
@@ -1,47 +1,14 @@
-open Images
-open Tiff
-
 open Core.Std
 open Re2.Std
 
 let generate_vectors = 
-	List.map ~f:Vec_of_tiff.generate_brightness_vec 
-
-(* TODO: remove *)
-let _generate_vectors_deprecated img_files_li = 
-	let gray_colors = float(256) in
-	let get_rgb_brightness  c = float(Color.brightness c) /. gray_colors in
-	let get_rgba_brightness c = float(Color.brightness c.color) /. gray_colors in
-	let get_gray_brightness c = float(c) /. gray_colors in
-	let combine getter brightness x y =
-		let c = getter x y in
-			brightness c
-	in
-	let f fname =
-		printf "Making vector from image: [%s]\n" fname;
-		let img = Images.load fname [] in
-		let w,h = Images.size img in
-		let format = ref "na" in
-		let getter =
-			match img with
-			| Index8 im  -> format := "index8"  ; combine (Index8.get im) get_gray_brightness
-			| Index16 im -> format := "index16" ; combine (Index16.get im) get_gray_brightness
-			| Rgb24 im   -> format := "rgb"     ; combine (Rgb24.get im) get_rgb_brightness
-			| Rgba32 im  -> format := "rgba"    ; combine (Rgba32.get im) get_rgba_brightness
-			| _ -> failwith (sprintf "unsupported image format for file: %s" fname) 
-		in
-		let arr = Array.init (w * h) ~f:(fun i -> getter (i % w) (i / w) )
-		in
-			(w,h,!format,arr)
-	in 
-		List.map ~f img_files_li
-
+	List.map ~f:(fun (i, fl) -> i, Vec_of_tiff.generate_brightness_vec fl) 
 
 let store_vectors file vectors =
 	let f ch =
 		List.iter
-			~f:(fun (w,h,fmt,arr) ->
-				fprintf ch "%d,%d %s: " w h fmt;
+			~f:(fun (id, (w,h,fmt,arr)) ->
+				fprintf ch "%d %d,%d %s: " id w h fmt;
 				Array.iter ~f:(fun v -> fprintf ch "%3.6f " v) arr;
 				Out_channel.newline ch;
 			)
@@ -90,7 +57,7 @@ let make_subj_files_list folder subj frames_set =
 		sprintf "%s/%d_%d.tif" folder subj
 	in
 	let f acc el =
-		(make_fname el)::acc
+		(el, make_fname el)::acc
 	in
 		Set.fold ~init:[] ~f frames_set |> List.rev
 
@@ -98,7 +65,7 @@ let cmd =
 	let run folder_in folder_out () =
 		gen_subjects_lists folder_in
 			|> Hashtbl.to_alist
-			|> List.map ~f:(fun (subj, frames) -> (subj, (make_subj_files_list folder_in subj frames)) )
+			|> List.map ~f:(fun (subj, frames) -> (subj, make_subj_files_list folder_in subj frames) )
 			|> List.iter ~f:(fun (subj, frames_files) -> 
 								generate_vectors frames_files
 								|> store_vectors (sprintf "%s/%d.vecs" folder_out subj) )
