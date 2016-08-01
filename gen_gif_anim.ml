f1daa10991696e3db10f94cd5560ed7541e7a079
karitra <karev.alexandre@gmail.com>
2016-08-01 23:59:28 +0300

    fix resizing rounding

--- gen_gif_anim.ml
+++ gen_gif_anim.ml
@@ -33,30 +33,32 @@ let compose_cmd subj folder path_list delay output =
 		Buffer.add_string b 
 	in
 	let file_pfx = sprintf "%d_" subj in
-	let page_def = sprintf "-page %dx%d" gif_w gif_h in
+	let page_def = sprintf "-resize %dx%d\\!" gif_w gif_h in
 	let label_pfx = "-annotate +20+20 " in
-		add_buff (sprintf "-resize %dx%d " gif_w gif_h);
+		(* add_buff (sprintf "-size %dx%d " gif_w gif_h); *)
 		add_buff (sprintf "-delay %dx1000 " delay);
 		add_buff "-font Helvetiva -fill green -pointsize 16 ";
 		List.iter ~f:(fun i -> 
 			let i_plus = i + 1 in
-			add_buff (sprintf "%s %s \"subj: %d frame: %d\" %s/%s%d.tif " 
-				page_def label_pfx subj i_plus folder file_pfx i_plus) ) path_list;
+			add_buff (sprintf "%s %s \"subj: %d\" %s/%s%d.tif " 
+				page_def label_pfx subj folder file_pfx i_plus) ) path_list;
 		add_buff "-loop 0 ";
 		(* add_buff (sprintf "%s/%s " folder output); *)
 		add_buff output;
 		Buffer.contents b
 
 let () =
-	let run path_file output delay () =
+	let run path_file output delay dump () =
 		let subj = extract_subject path_file and
 			path = read_path_seq path_file and
 			folder = Filename.dirname path_file
 		in
 		let c = compose_cmd subj folder path delay output in
-			let to_crop = (String.length c) / 3 in
-				printf "CMD:\n\t%s\nlen: %d\n" (String.drop_suffix c to_crop) (String.length c);
-				Shell.sh "convert %s" c
+			if dump then
+				printf "CMD:\n\t%s\nlen: %d\n" c (String.length c)
+			else 
+				();
+			Shell.sh "convert %s" c
 	in
 	let cmd = 
 		Command.basic
@@ -66,6 +68,7 @@ let () =
 				+> anon ("path_file" %:string)
 				+> anon ("output_gif" %:string)
 				+> flag "-delay" (optional_with_default 120 int) ~doc:"ms inter frame delay"
+				+> flag "-dump" (no_arg) ~doc:" dump 'convert' command"
 			)
 			run
 	in
