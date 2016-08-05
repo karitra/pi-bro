522d01c1f5e3a243be7b3c1402151555df9bf3e7
karitra <karev.alexandre@gmail.com>
2016-08-05 18:35:35 +0300

    mask aplly script correction

--- apply.mask.sh
+++ apply.mask.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ASK_SFX='.amask.png'
+MASK_SFX='.amask.png'
 FRAME_SIZE='400x380\!'
 
 if [ $# -lt 2 ]; then
@@ -13,10 +13,9 @@ echo Running on $CPUs CPUS
 
 echo Preparing masks in folder $1
 
-find $1 -type f -name "*_mask.tif" | xargs -P $CPUs -I '{MASK}' sh -c 'FL=$(basename {MASK}) convert  -alpha Shape {MASK} -fill green -opaque white '$2'/${FL%.*}'$MASK_SFX
+find $1 -type f -name "*_mask.tif" | xargs -P $CPUs -I '{MASK}' sh -c 'FL=$(basename {MASK}); convert  -alpha Shape {MASK} -fill green -opaque white '$2'/${FL%.*}'$MASK_SFX
 
 echo Preparing image files in folder $2
 
-find $2 -type f -name "*.$MASK_SFX" | xargs -P $CPUs -I '{MASK}' sh -c 'FL=$(basename {MASK}); IMG_BASE=${FL%_mask'$MASK_SFX'}; composite -compose Multiply '$1'/${IMG_BASE}.tif {MASK} -resize '"$FRAME_SIZE $2"'/{IMG_BASE}.png'
-
+find $2 -type f -name "*.$MASK_SFX" | xargs -P $CPUs -I '{MASK}' sh -c 'FL=$(basename {MASK}); IMG_BASE=${FL%_mask'$MASK_SFX'}; composite -compose Multiply '$1'/${IMG_BASE}.tif {MASK} -resize '"$FRAME_SIZE $2"'/${IMG_BASE}.png'
 
