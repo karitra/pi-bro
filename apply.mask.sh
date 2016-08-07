eb510e4655f8ae80ed53b643e4a41c23d5c8c1c2
Alex <alex@ip-172-31-18-193.eu-central-1.compute.internal>
2016-08-07 19:38:40 +0000

    ./apply.mask.sh: updated with more intermediate stages

--- apply.mask.sh
+++ apply.mask.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 MASK_SFX='.amask.png'
-# FRAME_SIZE='400x380\!'
+LABELED_SFX='.labeled.png'
+#FRAME_SIZE='400x380\!'
 
 if [ $# -lt 2 ]; then
 	echo "Wrong number of parameters, should be: SRC_DIR DST_DIR"
@@ -13,9 +14,10 @@ echo Running on $CPUs CPUS
 
 echo Preparing masks in folder $1
 
-find $1 -type f -name "*_mask.tif" | xargs -P $CPUs -I '{MASK}' sh -c 'FL=$(basename {MASK}); convert  -alpha Shape {MASK} -fill '"'rgba(0,250,0,0.5)'"' -opaque white '$2'/${FL%.*}'$MASK_SFX
+find $1 -type f -name "*_mask.tif" | xargs -P $CPUs -I '{MASK}' sh -c 'FL=$(basename {MASK}); SUBJ=${FL%%_*}; P="{MASK}"; SRC_FILE="${P%_*}.tif"; STRIP_MASK=${FL%_*}; FRAME=${STRIP_MASK##*_}; convert  -alpha Shape {MASK} -fill "rgba(0,250,0,0.5)" -opaque white '$2'/${FL%.*}'$MASK_SFX'; convert -undercolor green -fill white -annotate +10+10 "Subj: $SUBJ $FRAME" ${SRC_FILE} '$2'/$STRIP_MASK'$LABELED_SFX
+
 
 echo Combine images files in folder $2
 
-find $2 -type f -name "*$MASK_SFX" | xargs -P $CPUs -I '{MASK}' sh -c 'FL=$(basename {MASK}); IMG_BASE=${FL%_mask'$MASK_SFX'}; composite -compose Multiply '$1'/${IMG_BASE}.tif {MASK} '"$2"'/${IMG_BASE}.png'
+find $2 -type f -name "*$MASK_SFX" | xargs -P $CPUs -I '{MASK}' sh -c 'FL=$(basename {MASK}); IMG_BASE="${FL%_mask'$MASK_SFX'}"; LABELED="${IMG_BASE}'$LABELED_SFX'"; composite -compose Multiply '"$2"'/$LABELED {MASK} '"$2"'/${IMG_BASE}.png'
 
--- run.solver.sh
+++ run.solver.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-CPUs=`nproc --all`
+# CPUs=`nproc --all`
+CPUs=1
 ROLLS="1_200_000"
 
 find $1 -name '*.vecs' | xargs -P "$CPUs" -I '{SUBJ}' sh -c "./optimal_seq.native -file {SUBJ} -rolls $ROLLS > {SUBJ}.path"
