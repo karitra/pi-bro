9b7766e404c6e58b25c66e17fe4a9594c0bab076
karitra <karev.alexandre@gmail.com>
2016-08-10 20:36:09 +0300

    update of gif generator script

--- scripts/generate.gifs.sh
+++ scripts/generate.gifs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 CPUs=`nproc --all`
-find $1 -name "*.vecs.hist.path" | xargs -P $CPUs -I "{PATHS_FILE}" ./gen_gif_anim.native {PATHS_FILE}  $2 $3 -delay 250
+
+find $1 -name "*.vecs.hist.path" | xargs -P $CPUs -I "{PATHS_FILE}" sh -c 'FL=$(basename {PATHS_FILE}); SUBJ=${FL%%_*}; ../gen_gif_anim.native {PATHS_FILE} '$2' '$3'/anim.${SUBJ}.gif -delay 230'
